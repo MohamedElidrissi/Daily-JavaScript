8343bbc44558531294bcff718b975b4ab4c5fabb
Mohamed Elidrissi <mohamed.dev43@gmail.com>
2019-08-05 11:41:02 +0100

    Add editorconfig and prettier configs

--- challenge-#1.js
+++ challenge-#1.js
@@ -1,17 +1,17 @@
 // https://dev.to/thepracticaldev/daily-challenge-1-string-peeler-4nep
 // @author Mohamed Elidrissi
-const test = require("./test");
+const test = require('./test');
 
 function stringPeeler(string) {
   if (!string || string.length <= 2) {
     return null;
   }
-  const arr = string.split("");
+  const arr = string.split('');
   arr.splice(0, 1);
   arr.splice(-1, 1);
-  return arr.join("");
+  return arr.join('');
 }
 
-test(null, stringPeeler("hi"));
-test("ell", stringPeeler("Hello"));
-test("ohamed Elidriss", stringPeeler("Mohamed Elidrissi"));
+test(null, stringPeeler('hi'));
+test('ell', stringPeeler('Hello'));
+test('ohamed Elidriss', stringPeeler('Mohamed Elidrissi'));
